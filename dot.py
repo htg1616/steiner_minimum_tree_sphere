45d524eccbff488bef4caee397bf8b7edf739389
htg1616 <htg1616@gmail.com>
2024-09-21 16:52:23 +0900

    graph.py 수정 dot에 __copy__ 추가 LocalOptimizedGraph 아직 미완

--- dot.py
+++ dot.py
@@ -16,11 +16,14 @@ class Dot:
             math.cos(self.theta) * math.cos(other.theta) + math.sin(self.theta) * math.sin(other.theta) * math.cos(
                 self.phi - other.phi))
 
-    def __eq__(self, other):
+    def __eq__(self, other): #실수인데 부동소수점고려 해야함. ㅈ같네
         return self.theta == other.theta and self.phi == other.phi
 
     def __ne__(self, other):
         return self.theta != other.theta or self.phi != other.phi
+
+    def __copy__(self):
+        return Dot(theta=self.theta, phi=self.phi)
 
     def angle(self, other1, other2):
         #(self, other1)과 (self, other2)가 이루는 각 반환
--- graph.py
+++ graph.py
@@ -1,3 +1,4 @@
+from contributed.sumopy.agilepy.lib_wx.test_glcanvas import vertices
 from dot import *
 import copy
 
@@ -13,9 +14,10 @@ class MinimalSpanningTree:
                 self.edges.append((i, j))
 
         #mst 길이, 인접리스트, 간선
-        self.mst_length = None
         self.mst_adj_list = None
         self.mst_edges = None
+
+        self.kruskal()
 
     def kruskal(self):  # 크루스칼 알고리즘에 의해 생성된 최소 신장트리의 인접리스트를 반환
         def find(i):#부모 노드를 찾는 함수
@@ -34,32 +36,34 @@ class MinimalSpanningTree:
         # mst 인접리스트, 간선, 부모노드, 비용 초기화
         self.mst_adj_list = [[] for _ in range(len(self.vertices))]
         self.mst_edges = []
-        total_cost = 0
         parent = [i for i in range(len(self.vertices))]
         #크루스칼 알고리즘
-        self.edges.sort(key=lambda x: x[1] - x[0])
-        for cost, a, b in self.edges:
+        self.edges.sort(key=lambda x: vertices[x[1]] - vertices[x[0]])
+        for a, b in self.edges:
             if find(a) != find(b):
                 self.mst_adj_list[a].append(b)
+                self.mst_adj_list[b].append(a)
                 self.mst_edges.append((a, b))
+                self.mst_edges.append((b, a))
                 union(a, b)
-                total_cost += cost
 
-        self.mst_length = total_cost
-
+    def length(self):
+        length = 0
+        for a, b in self.mst_edges:
+            length += vertices[a] - vertices[b]
+        return length / 2
 
 #MinimalSpanningTree를 상속받은 steinertree class
 class SteinerTree(MinimalSpanningTree):
     def __init__(self, vertices: list):
         #MinimalSpanningTree의 생성자를 호출하고 크루스칼 알고리즘을 통해 mst를 생성
         super().__init__(vertices)
-        self.kruskal()
 
-        #steiner tree의 인접 리스트, 간선, 총 길이, steiner point를 포함한 전체 정점 리스트
-        self.si_adj_list = None
-        self.si_edges = None
-        self.si_length = None
-        self.si_vertices = None
+        self.si_vertices = None #SteinerTree의 좌표들 앞부분은 fixed points의 좌표, 뒷부분은 steiner points의 좌표
+        self.si_adj_list = None #SteinerTree의 인접리스트, si_vertices의 인덱스로 구성됨
+        self.si_edges = None #StwinerTree의 간선 표현, si_vertices의 인덱스로 구성됨
+
+        self.steiner_insertion()
 
     def find_min_angle_point(self, x, y):
         #edge (t_x, t_y)와 (t_y, t_z)가 가장 작은 각을 이루는 z 찾기, z는 스타이너 포인트도 될 수 있음
@@ -69,8 +73,8 @@ class SteinerTree(MinimalSpanningTree):
         min_point_index = None
         min_angel = 2 * math.pi #각도 최솟값을 2pi로 초기화
         #y의 모든 인접점에 대해  최소각을 가지는 z를 찾음
-        for z in self.mst_adj_list[point_y]:
-            point = self.vertices[z]
+        for z in self.si_adj_list[point_y]:
+            point = self.si_vertices[z]
             if point != point_x and point_y.angle(point_x, point) < min_angel:
                 min_angel = point_y.angle(point_x, point)
                 min_point_index = z
@@ -81,13 +85,13 @@ class SteinerTree(MinimalSpanningTree):
         #steiner insertion 알고리즘
 
         self.si_adj_list = copy.deepcopy(self.mst_adj_list)#기존 mst 인접리스트 복사
-        self.si_vertices = []
+        self.si_vertices = self.vertices[:]
 
-        for x, y in self.mst_edges:#모든 mst 간선에 대해 반복
-            point_x = self.vertices[x]
-            point_y = self.vertices[y]
-            z = self.find_min_angle_point(x, y)#x,y 각도가 작은 각을 이루는 z찾기
-            point_z = self.vertices[z]
+        for x, y in self.mst_edges: #모든 mst 간선에 대해 반복
+            point_x = self.si_vertices[x]
+            point_y = self.si_vertices[y]
+            z = self.find_min_angle_point(x, y) #x,y 각도가 작은 각을 이루는 z찾기
+            point_z = self.si_vertices[z]
 
             #z가 존재하고, x, y, z가 이루는 각이 120도보다 작으면 steiner point를 추가
             if not z and point_y.angle(point_x, point_z) < math.pi * 2 / 3:
@@ -105,4 +109,7 @@ class SteinerTree(MinimalSpanningTree):
                 self.si_adj_list[y].append(s_index)
                 self.si_adj_list[z].append(s_index)
 
-            
+
+class LocalOptimizedGraph(SteinerTree):
+    def __init__(self, vertices: list):
+        super().__init__(vertices) #mst와 si-insertion을 이용하여 스타이너 트리의 위상 생성
